7470696fbc1131d762bc73b7b6b4b4a121cf9df0
Darrk666 <damean@live.co.uk>
2016-04-01 12:59:30 +0100

    Added files via upload

---
+++ Purge_And_Remove_Domain.ps1
@@ -0,0 +1,8 @@
+﻿$Credential = Get-Credential
+Import-Module MsOnline
+Connect-MsolService -Credential $Credential
+$Domain = Read-Host 'Domain Name'
+Get-MsolUser -DomainName $Domain | %{Remove-MsolUser -UserPrincipalName $_.UserPrincipalName -Force}
+Get-MsolContact -All | ?{$_.ProxyAddresses -Match $Domain} | %{Remove-MsolContact -Objectid $_.Objectid -Force}
+Get-MsolGroup -All | ?{$_.ProxyAddresses -Match $Domain} | %{Remove-MsolGroup -Objectid $_.Objectid -Force}
+Remove-MsolDomain -DomainName $Domain 
